f6285d511278ae4588d044be37a356ed6225c2c8
VyrTtZ <tarkagustaw@gmail.com>
2026-02-24 10:14:55 +0000

    tried to set up the atabase

--- ForKostas/updatedStylidasSIGMA GAMIN/frontend/src/App.jsx
+++ ForKostas/updatedStylidasSIGMA GAMIN/frontend/src/App.jsx
@@ -5,7 +5,21 @@ import Router from "./router";
 import Header from "./components/common/Header";
 
 export default function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  var [isLoggedIn, setIsLoggedIn] = useState(false);
+  const handleLogin = async () => {
+  const response = await fetch("http://localhost:5000/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password })
+  });
+
+  const data = await response.json();
+  if (data.isLoggedIn) {
+    setIsLoggedIn(true);
+  }
+  };
+
+
   return (
     <BrowserRouter>
       <Header isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
